8b06539e85cb340f0624700c9a8a91b3c2716391
1T0S <tliberty@protonmail.com>
2020-10-22 21:20:05 +0200

    Initial commit

---
+++ src/init/Controller.java
@@ -0,0 +1,13 @@
+package init;
+
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class Controller {
+    private HBox hboxBottom, hboxTop;
+    private VBox vboxLeft;
+    private AnchorPane paneRight;
+    private MenuBar menu;
+}
